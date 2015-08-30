90f6ff2f7bdea766d259efc1e7f2076a02b55bdf
Calvin Lui <clui951@berkeley.edu>
2015-08-30 01:46:15 +0000

    wc.c handles stdin

--- hw0/wc.c
+++ hw0/wc.c
@@ -22,11 +22,12 @@ int main(int argc, char *argv[]) {
 	noWordBefore = TRUE;
 	buffer = argv[1];
 
-	fp = fopen ( argv[1] , "r" );
-	c = fgetc(fp);
+	fp = fopen( argv[1] , "r" );
 
+	if (buffer == 0x0) {
+		c = getchar();
 
-	while (c != -1) {
+		while (c != -1) {
 		if (c == 10) {
 			// new line character
 			noWordBefore = TRUE;
@@ -44,10 +45,38 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		charCount++;
+		c = getchar();
+		}
+		printf("%ld %ld %ld \n", lineCount, wordCount, charCount);
+	} else {
+
 		c = fgetc(fp);
+
+
+		while (c != -1) {
+			if (c == 10) {
+				// new line character
+				noWordBefore = TRUE;
+				lineCount++;
+			} else {
+				// there is a character (includes spaces)
+				if (c == 32) {
+					// space
+					noWordBefore = TRUE;
+				} else {
+					if (noWordBefore == TRUE) {
+						noWordBefore = FALSE;
+						wordCount++;
+					}
+				}
+			}
+			charCount++;
+			c = fgetc(fp);
+		}
+		fclose(fp);
+		printf("%ld %ld %ld %s \n", lineCount, wordCount, charCount, argv[1]);
 	}
 
-	printf("%ld %ld %ld %s \n", lineCount, wordCount, charCount, argv[1]);
 
     return 0;
 }
