96baa9d1de9887331f6869155eb596daf9f2acfc
Calvin Lui <clui951@berkeley.edu>
2015-08-30 07:29:17 +0000

    rlimit_data probs

--- hw0/main.c
+++ hw0/main.c
@@ -5,8 +5,10 @@ int main() {
     struct rlimit lim;
     getrlimit( RLIMIT_STACK , &lim);
     printf("stack size: %ld\n", (long int) lim.rlim_cur);
-    getrlimit( RLIMIT_MEMLOCK, &lim);
-    printf("process limit: %ld\n", (long int) lim.rlim_cur);
+    struct rlimit limm;
+    getrlimit( RLIMIT_DATA, &limm);
+    limm.rlim_cur = 64;
+    printf("process limit: %ld\n", (long int) limm.rlim_cur);
     getrlimit( RLIMIT_NOFILE, &lim);
     printf("max file descriptors: %ld\n", (long int) lim.rlim_cur);
     return 0;
