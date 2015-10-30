7ba1f436206dc8907379c89968d0b987eaa1ca74
Calvin Lui <clui951@berkeley.edu>
2015-10-30 14:10:47 +0000

    test13

--- hw3/mm_alloc.c
+++ hw3/mm_alloc.c
@@ -55,7 +55,7 @@ void *mm_malloc(size_t size) {
     		// need to split check
 
     		if (block->size == size) {
-    			memset(block, 0, block->size);
+    			memset(block->data, 0, block->size);
     			block->free = 0;
     		} else {
     			// size_t entireSize = block->size;
@@ -64,7 +64,7 @@ void *mm_malloc(size_t size) {
 				// set_contents(block, block + sizeof(struct s_block) + size, block->prev, 0, size);
 				// set_contents(block + sizeof(struct s_block) + size, entireblocknext, block, 1, second_size);
 
-	    		memset(block, 0, block->size);
+	    		memset(block->data, 0, block->size);
 	    		block->free = 0;
     		}
     	}
