662854d802e17ea6eceee70e3b552791f03d61d1
Calvin Lui <clui951@berkeley.edu>
2015-10-30 12:11:05 +0000

    first test

--- hw3/mm_alloc.h
+++ hw3/mm_alloc.h
@@ -7,6 +7,29 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/* block struct */
+struct s_block {
+	struct s_block *prev;
+	struct s_block *next;
+	int free;
+	size_t size;
+	char data[0];
+};
+
+/* attempts to split a block, with size_t as first */
+void split_block(struct s_block *b, size_t s);
+
+/* attempts to combine block with neighbors when free */
+struct s_block * combine_block(struct s_block *b);
+
+/* add to heap */
+struct s_block * extend_heap(struct s_block *prev_end, size_t s);
+
+void set_contents(struct s_block *b, struct s_block *next, struct s_block *prev, int free, size_t s);
 
 void *mm_malloc(size_t size);
 void *mm_realloc(void *ptr, size_t size);
